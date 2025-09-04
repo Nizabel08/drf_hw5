b879be190117e9b6f2bfda0d8ae9fd3ea0e4295c
Nizabel08 <Nizabel2019>
2025-09-04 20:02:43 +0400

    add app

--- projectHW/app/admin.py
+++ projectHW/app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.utils import timezone
+from .models import User
 
-# Register your models here.
+admin.site.site_header = "Nizabel's site"
+admin.site.site_title = "User registration"
+admin.site.index_title = "User portal"
+
+
+@admin.action(description='Mark selected users as admin')
+def make_admin(modeladmin, request, queryset) :
+    queryset.update(is_admin = True)
+
+
+@admin.action(description='Show only users that were created today')
+def date_filtering(modeladmin, request, queryset) :
+    today = timezone.now().date()
+    return queryset.filter(created_at_date = today)
+
+
+@admin.register(User) 
+class UserAdmin(admin.ModelAdmin) :
+    list_display = ('name', 'last_name', 'age', 'created_at', 'is_admin')
+    list_filter = ('age', 'is_admin', 'created_at')
+    search_fields = ('name', 'last_name')
+    ordering = ('name',)
+    list_editable = ('age',)
+    actions = [make_admin, date_filtering]
+
+
+ 
